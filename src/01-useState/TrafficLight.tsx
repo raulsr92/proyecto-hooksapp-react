6c3c98912b3309145f83a0e3f18ca2fe190935dd
Raul Sanchez R. <raul.sr92@gmail.com>
2026-07-06 18:37:49 -0500

    Clase 118 useState - Estado que re-dibuja

---
+++ src/01-useState/TrafficLight.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react"
+
+
+const colors = {
+    red: 'bg-red-500 animate-pulse',
+    yellow: 'bg-yellow-500 animate-pulse',
+    green: 'bg-green-500 animate-pulse',
+    default: 'bg-gray-500'
+}
+
+const TrafficLight = () => {
+
+    const [light, setLight] =  useState('red');
+
+    const handlerColorChange = ( color:string)=>{
+
+        setLight((prev)=>{
+            console.log({prev})
+            return color
+        })
+    }
+
+    return (
+        <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
+        <div className="flex flex-col items-center space-y-8">
+
+            <div className={`w-32 h-32 ${ light=="red"? colors[light] : colors.default } rounded-full`}></div>
+
+            <div className={`w-32 h-32 ${ light=="yellow"? colors[light] : colors.default } rounded-full`}></div>
+            <div className={`w-32 h-32 ${ light=="green"? colors[light] : colors.default } rounded-full`}></div>
+
+            {/* Botón para cambiar el estado de la luz */}
+            <div className="flex gap-2">
+            <button
+                className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer"
+                onClick={()=> handlerColorChange('red')}>
+                Rojo
+            </button>
+            <button
+                className="bg-yellow-500 text-white px-4 py-2 rounded-md cursor-pointer"
+                onClick={()=>{handlerColorChange('yellow')}}>
+                Amarillo
+            </button>
+            <button
+                className="bg-green-500 text-white px-4 py-2 rounded-md cursor-pointer"
+                onClick={()=>{handlerColorChange('green')}}>
+                Verde
+            </button>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default TrafficLight
